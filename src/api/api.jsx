ac04b5a350b83662a6143f1232af2525a8f0300e
aymen ELFADILI <aymenelfadili0@gmail.com>
2026-07-20 11:32:39 +0100

    final commit

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -5,7 +5,7 @@ const api = axios.create({
 });
 
 api.interceptors.request.use((config) => {
-  const token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbjEiLCJyb2xlIjoiQURNSU4iLCJpYXQiOjE3ODQyNzkxNzUsImV4cCI6MTc4NDMxNTE3NX0.eUTYK25Pvw0w19zq-Pgv8tAP3DLarELF_fhst6KG5tKvIUWJkD3Jfm2cM9W5VLCTrJC9TAtiEpOhjKo7OF8gtw";
+  const token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbjEiLCJyb2xlIjoiQURNSU4iLCJpYXQiOjE3ODQ1Mzc5NDIsImV4cCI6MTc4NDU3Mzk0Mn0.ZhYBOV4SeFOggyb8WYQXemlIEjx9nbJRx1_gRH_9kOPUN8eOzEdDISSrX93FWekToKQT06H2qTQ-N3wZ3epJfA";
 
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
