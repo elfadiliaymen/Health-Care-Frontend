77cc0bd0d65e745744b3704d95257af65e3f2c11
aymen ELFADILI <aymenelfadili0@gmail.com>
2026-07-22 11:51:51 +0100

    change token

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -5,7 +5,7 @@ const api = axios.create({
 });
 
 api.interceptors.request.use((config) => {
-  const token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbjEiLCJyb2xlIjoiQURNSU4iLCJpYXQiOjE3ODQ1Mzc5NDIsImV4cCI6MTc4NDU3Mzk0Mn0.ZhYBOV4SeFOggyb8WYQXemlIEjx9nbJRx1_gRH_9kOPUN8eOzEdDISSrX93FWekToKQT06H2qTQ-N3wZ3epJfA";
+  const token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbjEiLCJyb2xlIjoiQURNSU4iLCJpYXQiOjE3ODQ2NDI1MzIsImV4cCI6MTc4NDY3ODUzMn0.klkH67Y2J8vhZZ-jmlP9c6cyanpsjWoFmumpqViJe7QNoEVk__Uq5yy3PqeT-sMJ7kCXKNLPnAyXVmKg_tjLxA";
 
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
